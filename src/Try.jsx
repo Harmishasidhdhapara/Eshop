0049bc3688494014fb25879001261e24bbadd0af
Harmishasidhdhapara <harmishasidhdhapara@gmail.com>
2024-10-08 16:18:55 +0530

    hiii

---
+++ src/Try.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Try() {
+  return (
+    <>
+    <div>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis vel nam aut fugiat, nemo repellat, voluptatem facilis maxime temporibus repudiandae magnam cumque vitae adipisci atque, voluptatibus commodi ratione at deserunt.
+    </div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque repellendus, explicabo illo deleniti, odio officia nemo laudantium a ipsam eius iure esse rerum non. Magnam ullam recusandae cum hic? Ab!</div>
+    </>
+  )
+}
+
+export default Try
